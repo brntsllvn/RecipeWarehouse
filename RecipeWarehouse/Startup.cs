0a14519011c32d044332294d2cefd52cc5cbdde9
Brent Sullivan <brntsllvn@gmail.com>
2016-06-22 15:38:12 -0700

    Add project files.

---
+++ RecipeWarehouse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipeWarehouse.Startup))]
+namespace RecipeWarehouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
