107b305c56e2c0c5c5b411c24ca50e177386b21e
Brent Sullivan <brntsllvn@gmail.com>
2016-06-22 16:44:55 -0700

    Add recipe controller and model

---
+++ RecipeWarehouse.Tests/Controllers/RecipesControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using RecipeWarehouse.Controllers;
+
+namespace RecipeWarehouse.Tests.Controllers
+{
+    [TestFixture]
+    class RecipesControllerTests
+    {
+        [Test]
+        public void RecipeIndexIsNotNull()
+        {
+            var recipesController = new RecipesController();
+        }
+    }
+}
---
+++ RecipeWarehouse/Controllers/RecipesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using RecipeWarehouse.Models;
+
+namespace RecipeWarehouse.Controllers
+{
+    public class RecipesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new List<Recipe>());
+        }
+    }
+}
